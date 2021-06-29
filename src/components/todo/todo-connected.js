27bfa8db429e9a65d166750747b63383bf1a38d0
furatmalkawi29 <malkawifurat29@gmail.com>
2021-06-30 02:20:24 +0300

    ok

--- src/components/todo/todo-connected.js
+++ src/components/todo/todo-connected.js
@@ -1,68 +1,86 @@
 import React, { useEffect, useState } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
+import useAjax from '../../hooks/useAjax'
 
 import './todo.scss';
 
-const todoAPI = 'https://api-js401.herokuapp.com/api/v1/todo';
+const todoAPI = 'https://furat-api-server.herokuapp.com/api/v1/todo';
 
 
 const ToDo = () => {
-
   const [list, setList] = useState([]);
+  const [hitApi] = useAjax();
+  const options = {mode: 'cors',
+    cache: 'no-cache',
+    headers: { 'Content-Type': 'application/json' }}
 
-  const _addItem = (item) => {
-    item.due = new Date();
-    fetch(todoAPI, {
-      method: 'post',
-      mode: 'cors',
-      cache: 'no-cache',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(item)
-    })
-      .then(response => response.json())
-      .then(savedItem => {
-        setList([...list, savedItem])
-      })
-      .catch(console.error);
+
+  const _addItem = async (item) => {
+    
+    try{item.due = new Date();
+   
+    let  data = await hitApi('post',todoAPI,item,options);
+    setList([...list,data]);
+  }catch(error){
+    console.error(error);
+  }
   };
 
-  const _toggleComplete = id => {
 
+  const deleteItem = async (id) =>{
+    try{
+      let response;
     let item = list.filter(i => i._id === id)[0] || {};
+
+    let data;
+    if (item._id) {
+
+    let url = `${todoAPI}/${id}`;
+    data = await hitApi('delete',url,{},{mode: 'cors'});
+    }
+
+        let myList = list.filter(listItem => listItem._id !== data._id );
+        setList(myList);
+            }catch(error){
+        console.error(error);
+
+    }   
+}
+  
+
+  const _toggleComplete = async id => {
+
+    try{
+    let item = list.filter(i => i._id === id)[0] || {};
+    let data;
 
     if (item._id) {
 
       item.complete = !item.complete;
 
       let url = `${todoAPI}/${id}`;
-
-      fetch(url, {
-        method: 'put',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(item)
-      })
-        .then(response => response.json())
-        .then(savedItem => {
-          setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
-        })
-        .catch(console.error);
+      data = await hitApi('put',url,item,options);
+    }
+      
+    setList(list.map(listItem => listItem._id === item._id ? data : listItem));
+    }
+      catch(error){
     }
   };
 
-  const _getTodoItems = () => {
-    fetch(todoAPI, {
-      method: 'get',
-      mode: 'cors',
-    })
-      .then(data => data.json())
-      .then(data => setList(data.results))
-      .catch(console.error);
-  };
+
+   const _getTodoItems = async () => {
+     try{
+     let data = await hitApi('get',todoAPI,{},{mode: 'cors'});
+     setList(data);
+    }catch(error){
+      console.error(error);
+    }
+   };
 
   useEffect(_getTodoItems, []);
+
 
   return (
     <>
@@ -79,9 +97,10 @@ const ToDo = () => {
         </div>
 
         <div>
+          {/* {console.log(list)} */}
           <TodoList
             list={list}
-            handleComplete={_toggleComplete}
+            handleComplete={_toggleComplete} deleteItem={deleteItem}
           />
         </div>
       </section>
